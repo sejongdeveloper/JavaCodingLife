d2d20179c19e66060afc2c5018a35fd209361c46
5CLASS-184 <5CLASS-184@BIT05-02>
2019-06-20 15:42:17 +0900

    github 최초 업로트 테스트입니다.

--- Study/src/testNet/ServerEx4.java
+++ Study/src/testNet/ServerEx4.java
@@ -7,8 +7,6 @@ public class ServerEx4 {
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = new ServerSocket(5000);
 		System.out.println("서버시작");
-		while(true) {
-			
-		}
+		
 	}
 }
