71dd6237a568c39f9ac0ea92c65ae9745c6dfdc6
sejon <sejon@211.227.8.231>
2019-06-23 00:25:42 +0900

    JavaFx Project

---
+++ src/kang/N1.java
@@ -0,0 +1,10 @@
+package kang;
+
+public class N1 {
+	public static void main(String[] args) {
+		int n = 12345;
+		System.out.println(n / 60 / 60);
+		System.out.println((n / 60) % 60);
+		System.out.println(n % 60);
+	}
+}
